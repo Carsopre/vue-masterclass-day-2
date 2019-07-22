d719ba091a212e98669e6c6fe215810fb91b619f
Carles Soriano Perez <cssp1989@gmail.com>
2019-07-22 10:24:39 +0200

    added emoji picker component

--- src/components/emoji-picker.vue
+++ src/components/emoji-picker.vue
@@ -1,1 +1,47 @@
-// TODO: create your single file component here
+<template>
+    <div class="emoji-picker">
+        <button 
+            v-for="emoji in emojis"
+            :key="emoji"
+            @click="onSelectedEmoji(emoji)"
+            type="button"
+            >
+            {{emoji}}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+        emojis: [
+            "😀",
+            "😂",
+            "😁"
+        ]
+    };
+  },
+  methods: {
+    onSelectedEmoji(selected) {
+      if (selected !== "") {
+        this.$emit("pick", selected);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.emoji-picker {
+  -webkit-appearance: none;
+  background: none;
+  outline: none;
+  font: inherit;
+  /* flex: 1; */
+  border-radius: 5px;
+  border: 2px solid var(--secondary-color);
+  padding: 0.1em 0.1em;
+}
+
+</style>

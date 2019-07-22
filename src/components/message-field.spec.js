cebb8e7833bd5251d99d2d7243d71f97dedcd0bf
Carles Soriano Perez <cssp1989@gmail.com>
2019-07-22 15:34:05 +0200

    added unit and integration test

--- src/components/message-field.spec.js
+++ src/components/message-field.spec.js
@@ -1,7 +1,39 @@
+import Vue from 'vue'
 import { mount } from '@vue/test-utils';
+import MessageField from './message-field';
 
 it('emits the message when pressing enter', () => {
+    // 1. set test data
+    const wrapper = mount(MessageField);
+    const testValue = "dummyTest";
+    const expectedEmit = "submit";
+    
+    // 2. Set expectations
+    wrapper.setData({text: testValue});
+    
+    // 3. Run test
+    wrapper.find('form').trigger('submit')
+    
+    // 4. verify final expectations.
+    expect(wrapper.emitted(expectedEmit)).toBeTruthy();
+    expect(wrapper.emitted(expectedEmit)).toEqual([[testValue]]);
+});
 
+it('emits the message when onSubmit', () => {
+    // 1. set test data
+    const wrapper = mount(MessageField);
+    const testValue = "dummyTest";
+    const expectedEmit = "submit";
+    
+    // 2. Set expectations
+    wrapper.setData({text: testValue});
+    
+    // 3. Run test
+    wrapper.vm.onSubmit();
+    
+    // 4. verify final expectations.
+    expect(wrapper.emitted(expectedEmit)).toBeTruthy();
+    expect(wrapper.emitted(expectedEmit)).toEqual([[testValue]]);
 });
 
 it('allows you to add emojis', () => {
@@ -9,5 +41,17 @@ it('allows you to add emojis', () => {
 });
 
 it('clears the text after it has been submitted', () => {
-
+    // 1. set test data
+    const wrapper = mount(MessageField);
+    const testValue = "dummyTest";
+    const expectedEmit = "submit";
+    
+    // 2. Set expectations
+    wrapper.setData({text: testValue});
+    
+    // 3. Run test
+    wrapper.find('form').trigger('submit')
+    
+    // 4. verify final expectations.
+    expect(wrapper.emitted(expectedEmit)).toBeTruthy();
 });

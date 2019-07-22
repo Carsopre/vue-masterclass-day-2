943161e5234b5d6961abf94e77cad458ac50f69c
Carles Soriano Perez <cssp1989@gmail.com>
2019-07-22 11:33:28 +0200

    Defined store.js based on example given, added to main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 import './main.css'
 
 Vue.config.productionTip = false
@@ -26,5 +27,5 @@ new Vue({
       console.log(`messages changed to ${newValue}`);
     }
   },
-  //TODO: add store
+  store
 }).$mount('#app')
--- src/store.js
+++ src/store.js
@@ -1,5 +1,20 @@
 // TODO: import Vue and Vuex
+import Vue from 'vue'
+import Vuex from 'vuex'
 
 // TODO: install Vuex to Vue
+Vue.use(Vuex);
+const store = new Vuex.Store({
+    state:{
+        messages: [],
+        user: {},
+    },
+    mutations:{
+
+    },
+
+    strict: true,
+});
 
 // TODO: Export store object with messages [] and user {} in it
+export default store
